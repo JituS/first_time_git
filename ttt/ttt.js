455fe641223a7bd10031aece7e66646bb6335497
Jitendra <jitendrs@thoughtworks.com>
2015-10-26 18:04:42 +0530

    Tic-Tac-Toe completed

--- ttt/ttt.js
+++ ttt/ttt.js
@@ -1,14 +1,18 @@
 var lib = require("./tttLib.js").lib;
 var axel = require("axel");
-axel.clear();
+
+var instruction = "............instruction............\n\t-------------------------------------------\n\n1	This game is made for two players (player X and player 0)\n2	turn can be played like [  00  ,  01  ,  02  ,  10  ,  11  ,  12  ,  20  ,  21  ,  22  ]";
+
 
 process.stdin.setEncoding("utf8")
 
-process.stdout.cursorTo(20,3);
 var board = lib.getBoard();
 
 process.stdin.on("readable",function(){
 	axel.clear();
+	process.stdout.cursorTo(10,1)
+	axel.fg(222,19,222);
+	console.log(instruction);
 	var turn = process.stdin.read();
 	if(turn!=null){
 		board = lib.writeTurn(board,turn);
--- ttt/tttLib.js
+++ ttt/tttLib.js
@@ -1,7 +1,17 @@
 var lib = {};
 exports.lib = lib;
 var chances = 0;
-var player1 = "0",player2 = "X";
+var players = {player1:{symbol:"0",chances:[]},player2:{symbol:"X",chances:[]}};
+
+var winningChancesOfPlayer1 = [["00","01","02"],
+					  ["10","11","12"],
+					  ["20","21","22"],
+					  ["00","11","22"],
+					  ["02","11","20"],
+					  ["00","10","20"],
+					  ["01","11","21"],
+					  ["02","12","22"]];
+var winningChancesOfPlayer2 = JSON.parse(JSON.stringify(winningChancesOfPlayer1));
 
 lib.getBoard = function(){
 	var board = new Array(4).join("*").split("");
@@ -19,35 +29,57 @@ lib.checkPosition = function(board,turn){
 	return false;
 };
 
-lib.checkWinner = function(board){
-	if((board[0][0] == board[1][0] && board[1][0] == board[2][0])){
-		console.log("win");
-		process.exit();
+lib.checkWinner = function(player,chance,board){
+	for(var i = 0;i < winningChancesOfPlayer1.length;i++){
+		for(var j = 0;j<winningChancesOfPlayer1[i].length;j++){
+			if(player.symbol == "0"){
+				winningChancesOfPlayer1[i][j] = (winningChancesOfPlayer1[i][j] == chance) ? "":winningChancesOfPlayer1[i][j];
+				if(winningChancesOfPlayer1[i].join("")=="") lib.showBoard(board),console.log("\nPlayer 0 Wins....."),process.exit(0); 
+			}else{
+				winningChancesOfPlayer2[i][j] = (winningChancesOfPlayer2[i][j] ==chance) ? "":winningChancesOfPlayer2[i][j];
+				if(winningChancesOfPlayer2[i].join("")=="") lib.showBoard(board),console.log("\nPlayer X Wins....."),process.exit(0); 
+			}
+		}
 	}
 };
 
 lib.writeTurn = function(board,turn,player){
 	if(lib.checkPosition(board,turn) && chances<10){
 		chances++;
-		board[turn[0]][turn[1]] = (chances%2) ? player1 : player2;
+		board[turn[0]][turn[1]] = (chances%2) ? players.player1.symbol : players.player2.symbol;
+		(chances%2) ? players.player1.chances.push(turn.slice(0,2)) : players.player2.chances.push(turn.slice(0,2));
+		(chances%2) ? lib.checkWinner(players.player1,turn.slice(0,2),board) : lib.checkWinner(players.player2,turn.slice(0,2),board);
+		if(board.join().match(/(\s)/g) == null) lib.showBoard(board),console.log("Drow...."), process.exit();
 		return board;
-	}
-	console.log("Please enter correct position");
+	};
+	process.stdout.cursorTo(20,20);
+	console.log("Please enter correct position.....");
 	return board;
-};
+};	
 
 var boardDesign = function(board){
 	var newBoard = board.map(function(each,i){
-		return each.join("  |  ");
+		return i+".  "+each.join(" |  ");
 	});
-	process.stdout.cursorTo(24,3);
-	console.log(newBoard.join("\n\t\t\t==============\n\t\t\t"))
+	var index=[];
+	for(var i=0;i<board.length;i++){
+		for(var j=0;j<board.length;j++){
+			if(board[i][j] == " ")
+				index.push([i,j]);
+		}
+	}
+	process.stdout.cursorTo(24,8);
+	console.log("  0     1     2");
+	process.stdout.cursorTo(20,9);
+	console.log("______________________\n")
+	process.stdout.cursorTo(24,11);
+	console.log(newBoard.join("\n\t\t\t================\n\t\t\t"))
 };
 
 
 lib.showBoard = function(board,player){
 	boardDesign(board);
-	process.stdout.cursorTo(20,10);
-	process.stdout.write(chances%2 ? "player X  ":"player 0  ")
+	process.stdout.cursorTo(20,17);
+	process.stdout.write(chances%2 ? "player X  turn:  ":"player 0  turn:  ");
 };	
 
