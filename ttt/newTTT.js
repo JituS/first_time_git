43d3db68b980ff3b6b284ba20b4eca10cccf4356
Jitendra <jitendrs@thoughtworks.com>
2015-10-27 16:09:48 +0530

    player option set

--- ttt/newTTT.js
+++ ttt/newTTT.js
@@ -9,23 +9,6 @@ var chancesCanBePlayed = {zeroZero:"00",
 	secondFirst:"21",
 	secondSecond:"22"};
 
-var getBoard = function(){
-	var board = new Array(4).join("*").split("");
-	board = board.map(function(each){
-		return new Array(4).join(" ").split("");
-	});
-	return board;
-};
-
-var board = getBoard();
-
-var main = function(turn){
-	board = writeTurn(board,turn);
-	boardDesign(board);
-}
-
-var players = {player1:{symbol:"0",chances:[]},player2:{symbol:"X",chances:[]}};
-
 var winningChancesOfPlayer1 = [["00","01","02"],
   ["10","11","12"],
   ["20","21","22"],
@@ -36,41 +19,57 @@ var winningChancesOfPlayer1 = [["00","01","02"],
   ["02","12","22"]];
 
 var winningChancesOfPlayer2 = JSON.parse(JSON.stringify(winningChancesOfPlayer1));
+var getBoard = function(){
+	var board = new Array(4).join("*").split("");
+	board = board.map(function(each){
+		return new Array(4).join(" ").split("");
+	});
+	return board;
+};
+var board = getBoard();
+var players = {player1:{symbol:"0",chances:[]},player2:{symbol:"X",chances:[]}};
+
+var main = function(turn){
+	board = writeTurn(board,turn);
+	boardDesign(board);
+}; 
+
 
 var checkPosition = function(board,turn){
-	var turn = turn.split("");
 	if(+turn[0]<=2 && +turn[1]<=2){
 		return board[turn[0]][turn[1]] == " ";
 	}
 	return false;
 };
 
-var checkWinner = function(player,chance,board){
+var checkWinner = function(player,turn,board){
 	for(var i = 0;i < winningChancesOfPlayer1.length;i++){
 		for(var j = 0;j<winningChancesOfPlayer1[i].length;j++){
-			if(player.symbol == "0"){
-				winningChancesOfPlayer1[i][j] = (winningChancesOfPlayer1[i][j] == chance) ? "":winningChancesOfPlayer1[i][j];
-				if(winningChancesOfPlayer1[i].join("")==""){
-					boardDesign(board);
-					document.getElementById("winText").innerHTML = "Player 0 wins......."; 
-				};
-			}else{
-				winningChancesOfPlayer2[i][j] = (winningChancesOfPlayer2[i][j] ==chance) ? "":winningChancesOfPlayer2[i][j];
-				if(winningChancesOfPlayer2[i].join("")=="") {
-					boardDesign(board);
-					document.getElementById("winText").innerHTML = "Player X wins......."; 
-				}
-			}
-		}
-	}
+			(player == "0")
+			? winningChancesOfPlayer1[i][j] = (winningChancesOfPlayer1[i][j] == turn) ? "":winningChancesOfPlayer1[i][j]
+			: winningChancesOfPlayer2[i][j] = (winningChancesOfPlayer2[i][j] ==turn) ? "":winningChancesOfPlayer2[i][j];
+			if(winningChancesOfPlayer1[i].join("")==""){
+				boardDesign(board);
+				document.getElementById("winText").innerHTML = "Player 0 wins......."; 
+				main =function(){};
+			};
+			if(winningChancesOfPlayer2[i].join("")==""){
+				boardDesign(board);
+				document.getElementById("winText").innerHTML = "Player X wins......."; 
+				main =function(){};
+			};
+		};
+	};
 };
 
+
 var writeTurn = function(board,turn){
-	if(checkPosition(board,turn) && chances < 9){
+	if(checkPosition(board,turn)){
 		chances++;
 		board[turn[0]][turn[1]] = (chances%2) ? players.player1.symbol : players.player2.symbol;
+		document.getElementById("turn").innerHTML = (chances%2) ? "Player X turn" : "Player 0 turn";
 		(chances%2) ? players.player1.chances.push(turn) : players.player2.chances.push(turn);
-		(chances%2) ? checkWinner(players.player1,turn,board) : checkWinner(players.player2,turn,board);
+		(chances%2) ? checkWinner(players.player1.symbol,turn,board) : checkWinner(players.player2,turn,board);
 		if(board.join().match(/(\s)/g) == null) boardDesign(board);
 		return board;
 	};
